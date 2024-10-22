7ba3ca0db69da1562ef0f45b310807995965d336
GitF1 <vinhpmde170405@fpt.edu.vn>
2024-10-22 21:11:08 +0700

    auth filter -M

--- src/java/controller/auth/LoginServlet.java
+++ src/java/controller/auth/LoginServlet.java
@@ -183,7 +183,7 @@ public class LoginServlet extends HttpServlet {
                 }
 
                 case "ADMIN" ->
-                    request.getRequestDispatcher("/admin").forward(request, response);
+                    response.sendRedirect(RouterURL.CREATE_MOVIE_SLOT );
             }
 
         } else {
