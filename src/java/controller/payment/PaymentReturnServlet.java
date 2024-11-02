bd3ffd97b7d1ae7375e8fbfa8d82e224644aef15
Nghia <nghiadang0602@gmail.com>
2024-11-02 10:58:35 +0700

    fixPayment

--- src/java/controller/payment/PaymentReturnServlet.java
+++ src/java/controller/payment/PaymentReturnServlet.java
@@ -4,6 +4,8 @@ import DAO.UserDAO;
 import DAO.payment.OrderDAO;
 import DAO.payment.PaymentDAO;
 import DAO.ticket.TicketDAO;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import model.BookingSession;
 import model.Order;
 import model.Seat;
@@ -20,6 +22,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,6 +39,7 @@ public class PaymentReturnServlet extends HttpServlet {
     private TicketDAO ticketDAO;
     private UserDAO userDAO;
     private static final Logger LOGGER = Logger.getLogger(PaymentReturnServlet.class.getName());
+    Gson gson = new Gson();
 
     @Override
     public void init() throws ServletException {
@@ -53,50 +59,40 @@ public class PaymentReturnServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                LOGGER.log(Level.INFO, "Cookie Name: {0}, Value: {1}", new Object[]{cookie.getName(), cookie.getValue()});
+                // existing switch cases...
+            }
+        }
 
         Integer userID = null;
         Double totalPrice = null;
-       
+        List<Seat> listSeats = parseSeatListFromCookie(request);
+        List<CanteenItemOrder> itemOrders = parseCanteenItemListFromCookie(request);
 
         HttpSession session = request.getSession();
-        BookingSession bookingSession = (BookingSession) session.getAttribute("bookingSession");
 
         String email = (String) session.getAttribute("email");
         String amountStr = request.getParameter("vnp_Amount");
         String vnp_ResponseCode = request.getParameter("vnp_ResponseCode");
         String vnp_TxnRef = request.getParameter("vnp_TxnRef");
-        List<Seat> listSeats = bookingSession.getListSeats();
-        List<CanteenItemOrder> itemOrders = bookingSession.getItemOrders();
         int orderID = Integer.parseInt(vnp_TxnRef);
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 switch (cookie.getName()) {
-                    case "orderID":
-                        orderID = Integer.parseInt(cookie.getValue());
-                        break;
                     case "userID":
                         userID = Integer.parseInt(cookie.getValue());
                         break;
                     case "totalPrice":
                         totalPrice = Double.parseDouble(cookie.getValue());
                         break;
+                    
                     default:
                         break;
                 }
             }
-        }
-
-        if (userID == null) {
-            LOGGER.log(Level.WARNING, "UserID not found in session.");
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "UserID not found in session!");
-            return;
-        }
-
-        if (amountStr == null || amountStr.isEmpty()) {
-            LOGGER.log(Level.WARNING, "Invalid amount received in VNPAY response.");
-            response.getWriter().write("Invalid amount!");
-            return;
         }
 
         if ("00".equals(vnp_ResponseCode)) {
@@ -107,10 +103,8 @@ public class PaymentReturnServlet extends HttpServlet {
                 orderDAO.updateOrderStatus(orderID, "success");
                 LOGGER.log(Level.INFO, "Order status updated to 'success' for OrderID: {0}", orderID);
 
-                for (Seat seat : listSeats) {
-                    ticketDAO.updateTicketStatus(orderID, seat.getSeatID(), "Success");
-                    LOGGER.log(Level.INFO, "Ticket status updated to 'Success' for SeatID: {0} in OrderID: {1}", new Object[]{seat.getSeatID(), orderID});
-                }
+                ticketDAO.updateTicketStatus(orderID, "Success");
+                LOGGER.log(Level.INFO, "Ticket status updated to 'Success' for SeatID: {0} in OrderID: {1}", new Object[]{orderID});
 
                 for (CanteenItemOrder itemOrder : itemOrders) {
                     if (itemOrder == null) {
@@ -173,10 +167,8 @@ public class PaymentReturnServlet extends HttpServlet {
             orderDAO.updateOrderStatus(orderID, "Failed");
             LOGGER.log(Level.INFO, "Order status updated to 'Failed' for OrderID: {0}", orderID);
 
-            for (Seat seat : listSeats) {
-                ticketDAO.updateTicketStatus(orderID, seat.getSeatID(), "Failed");
-                LOGGER.log(Level.INFO, "Ticket status updated to 'Failed' for SeatID: {0} in OrderID: {1}", new Object[]{seat.getSeatID(), orderID});
-            }
+            ticketDAO.updateTicketStatus(orderID, "Failed");
+            LOGGER.log(Level.INFO, "Ticket status updated to 'Failed' for SeatID: {0} in OrderID: {1}", new Object[]{orderID});
 
             request.setAttribute("message", "Failed");
         }
@@ -191,6 +183,54 @@ public class PaymentReturnServlet extends HttpServlet {
         request.setAttribute("vnp_TransactionStatus", request.getParameter("vnp_TransactionStatus"));
 
         request.getRequestDispatcher(RouterJSP.RETURN_TRACSACTION_BOOKING_TICKET).forward(request, response);
+    }
+
+    private List<Seat> parseSeatListFromCookie(HttpServletRequest request) {
+
+        List<Seat> seatListFromCookie = new ArrayList<>();
+
+        for (Cookie cookie : request.getCookies()) {
+            if ("listSeats".equals(cookie.getName())) {
+                try {
+                    // Giải mã chuỗi JSON từ cookie
+                    String decodedValue = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8.toString());
+
+                    // Chuyển đổi chuỗi JSON thành danh sách ghế
+                    List<Seat> listSeatsFromCookie = gson.fromJson(decodedValue, new TypeToken<List<Seat>>() {
+                    }.getType());
+
+                    // Ghi log danh sách ghế đã được lấy ra từ cookie
+                    LOGGER.log(Level.INFO, "Retrieved Seats from cookie: {0}", listSeatsFromCookie);
+                } catch (Exception e) {
+                    LOGGER.log(Level.WARNING, "Failed to parse seat list from cookie: {0}", e.getMessage());
+                }
+            }
+        }
+        return seatListFromCookie;
+    }
+
+    private List<CanteenItemOrder> parseCanteenItemListFromCookie(HttpServletRequest request) {
+
+        List<CanteenItemOrder> itemOrderListFromCookie = new ArrayList<>();
+
+        for (Cookie cookie : request.getCookies()) {
+            if ("listCanteenItems".equals(cookie.getName())) {
+                try {
+                    // Giải mã chuỗi JSON từ cookie
+                    String decodedValue = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8.toString());
+
+                    // Chuyển đổi chuỗi JSON thành danh sách các đối tượng CanteenItemOrder
+                    itemOrderListFromCookie = gson.fromJson(decodedValue, new TypeToken<List<CanteenItemOrder>>() {
+                    }.getType());
+
+                    // Ghi log danh sách item order đã được lấy ra từ cookie
+                    LOGGER.log(Level.INFO, "Retrieved Canteen Items from cookie: {0}", itemOrderListFromCookie);
+                } catch (Exception e) {
+                    LOGGER.log(Level.WARNING, "Failed to parse canteen item list from cookie: {0}", e.getMessage());
+                }
+            }
+        }
+        return itemOrderListFromCookie;
     }
 
     @Override
